e1107363dac7891880ab81fdeae64b9250bc8838
Steven Schultz <StevenSchultzA@gmail.com>
2016-11-18 13:29:02 -0500

    Sticky nav bar on mobile

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+  var nav = $('.nav');
+  var navScroll = 'nav-scrolled';
+  var headerHeight = $('.hero').height();
+  $(window).scroll(function(){
+    if($(this).scrollTop() > headerHeight)
+      nav.addClass(navScroll);
+    else
+      nav.removeClass(navScroll);
+  });
+
+  // Expand description of services
   $('.service-item').on('click', function(){
     $(this).find('.more').slideToggle();
   });
